1e67e67aabd261b77be6753ccf05260060e637fd
yuhanz1 <yuhanz9149@gmail.com>
2023-10-04 18:45:58 -0400

    a

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const height =
+const height = Number(prompt("enter your height in meters"));
+const weight = Number(prompt("enter your weight in kilograms"));
+
 function getBMI(){
-    
+    console.log(weight/height**2*100);
+}
+getBMI();
+/*
+function getBMI(){
 }
 
 function checkBMI(){
@@ -16,3 +22,21 @@ function results(){
 }
 
 // === checks type / == does not check for type
+*/
+
+
+/*
+function checkBMI(){
+    let BMI = getBMI(height, weight);
+    checkBMI(BMI);
+
+    if(getBMI < 18.5){
+        console.log("Eat more cake!");
+    } else if(getBMI < 24.9){
+        console.log("You're good to go");
+    } else(getBMI > 25); {
+        console.log("Try and cut your food intake a little :)")
+    }
+}
+checkBMI();
+*/
